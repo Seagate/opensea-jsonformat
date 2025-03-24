5af27dcd77d3056fc2a389f985fedb97363f6556
Nidhi Sharma <nidhi.sharma@seagate.com>
2025-03-25 01:29:58 +0530

    Quick : changes as per review comments

--- src/smart_attribute_json.c
+++ src/smart_attribute_json.c
@@ -38,10 +38,12 @@
 
 static void create_Node_For_SMART_Attribute(json_object* rootObject, ataSMARTAnalyzedAttribute smartAnalyzedAttribute)
 {
-#define MAX_ATTRIBUTE_NODE_NAME_LENGTH           21
-#define MAX_RAW_DATA_VALUE_IN_HEX_LENGTH         21
-#define MAX_RAW_VALUE_OF_INT64_IN_STRING_LENGHT  21
-#define MAX_RAW_VALUE_OF_DOUBLE_IN_STRING_LENGHT 21
+#define MAX_ATTRIBUTE_NODE_NAME_LENGTH   21
+#define MAX_RAW_DATA_VALUE_IN_HEX_LENGTH 17
+#define MAX_INT64_TO_DEC_STRING_LENGHT   21
+#define MAX_DOUBLE_TO_DEC_STRING_LENGHT  21
+#define MAX_UINT16_TO_HEX_STRING_LENGHT  7
+#define MAX_UINT8_TO_HEX_STRING_LENGHT   5
 
     json_object* attributeNode = json_object_new_object();
 
@@ -57,6 +59,12 @@ static void create_Node_For_SMART_Attribute(json_object* rootObject, ataSMARTAna
     }
 
     // add atribute type
+    json_object* statusNode = json_object_new_object();
+    // add the hex value
+    DECLARE_ZERO_INIT_ARRAY(char, statusValue, MAX_UINT16_TO_HEX_STRING_LENGHT);
+    snprintf_err_handle(statusValue, MAX_UINT16_TO_HEX_STRING_LENGHT, "0x%04" PRIX16 "", smartAnalyzedAttribute.status);
+    json_object_object_add(statusNode, "Status", json_object_new_string(statusValue));
+    // now add each attribute type set for this
     json_object* jarray = json_object_new_array();
     if (smartAnalyzedAttribute.attributeType.preFailAttribute)
     {
@@ -84,7 +92,8 @@ static void create_Node_For_SMART_Attribute(json_object* rootObject, ataSMARTAna
     {
         json_object_array_add(jarray, json_object_new_string("Self-Preserving. Saves between power cycles."));
     }
-    json_object_object_add(attributeNode, "Attribute Type(s)", jarray);
+    json_object_object_add(statusNode, "Attribute Type(s)", jarray);
+    json_object_object_add(attributeNode, "Status Information", statusNode);
 
     // add current(nominal) value, worst value
     json_object_object_add(attributeNode, "Current (Nominal) Value",
@@ -93,157 +102,132 @@ static void create_Node_For_SMART_Attribute(json_object* rootObject, ataSMARTAna
                            json_object_new_uint64(C_CAST(uint64_t, smartAnalyzedAttribute.worstEver)));
 
     // add threshold information
+    json_object* thresholdNode = json_object_new_object();
     switch (smartAnalyzedAttribute.thresholdInfo.thresholdType)
     {
     case THRESHOLD_ALWAYS_PASSING:
-        json_object_object_add(attributeNode, "Threshold", json_object_new_string("Set to always passing"));
+        json_object_object_add(thresholdNode, "Threshold Value", json_object_new_string("Set to always passing"));
         break;
     case THRESHOLD_ALWAYS_FAILING:
-        json_object_object_add(attributeNode, "Threshold", json_object_new_string("Set to always failing"));
+        json_object_object_add(thresholdNode, "Threshold Value", json_object_new_string("Set to always failing"));
         break;
     case THRESHOLD_INVALID:
-        json_object_object_add(attributeNode, "Threshold", json_object_new_string("Set to invalid value"));
+        json_object_object_add(thresholdNode, "Threshold Value", json_object_new_string("Set to invalid value"));
         break;
     case THRESHOLD_SET:
+    {
+        DECLARE_ZERO_INIT_ARRAY(char, thresholdValue, MAX_UINT8_TO_HEX_STRING_LENGHT);
+        snprintf_err_handle(thresholdValue, MAX_UINT8_TO_HEX_STRING_LENGHT, "0x%02" PRIX8 "",
+                            smartAnalyzedAttribute.thresholdInfo.thresholdValue);
+        json_object_object_add(thresholdNode, "Threshold Value", json_object_new_string(thresholdValue));
         if (smartAnalyzedAttribute.thresholdInfo.failStatus != FAIL_STATUS_NOT_SET)
         {
-            json_object* thresholdNode = json_object_new_object();
-            json_object_object_add(
-                thresholdNode, "Threshold Value",
-                json_object_new_uint64(C_CAST(uint64_t, smartAnalyzedAttribute.thresholdInfo.thresholdValue)));
             json_object_object_add(thresholdNode, "Fail Status",
                                    json_object_new_string(smartAnalyzedAttribute.thresholdInfo.failStatusString));
-            json_object_object_add(attributeNode, "Threshold Information", thresholdNode);
+        }
+    }
+    break;
+    case THRESHOLD_UNKNOWN:
+    default:
+        // should never get here
+        break;
+    }
+    json_object_object_add(attributeNode, "Threshold Information", thresholdNode);
+
+    // add raw data fields
+    json_object* rawDataNode = json_object_new_object();
+    // add the raw data bytes 0:7 in hex
+    DECLARE_ZERO_INIT_ARRAY(char, rawDataValue, MAX_RAW_DATA_VALUE_IN_HEX_LENGTH);
+    snprintf_err_handle(rawDataValue, MAX_RAW_DATA_VALUE_IN_HEX_LENGTH,
+                        "0x%02" PRIX8 "%02" PRIX8 "%02" PRIX8 "%02" PRIX8 "%02" PRIX8 "%02" PRIX8 "%02" PRIX8 "",
+                        smartAnalyzedAttribute.rawData.rawData[6], smartAnalyzedAttribute.rawData.rawData[5],
+                        smartAnalyzedAttribute.rawData.rawData[4], smartAnalyzedAttribute.rawData.rawData[3],
+                        smartAnalyzedAttribute.rawData.rawData[2], smartAnalyzedAttribute.rawData.rawData[1],
+                        smartAnalyzedAttribute.rawData.rawData[0]);
+    json_object_object_add(rawDataNode, "Raw Data", json_object_new_string(rawDataValue));
+    // add raw fields
+    for (uint8_t field = 0; field < smartAnalyzedAttribute.rawData.userFieldCount; field++)
+    {
+        char* unitString = M_REINTERPRET_CAST(char*, safe_calloc(MAX_RAW_FEILD_UNIT_STRING_LENGTH, sizeof(char)));
+        get_Raw_Field_Unit_String(smartAnalyzedAttribute.rawData.rawField[field].fieldUnit, &unitString);
+        if (safe_strlen(unitString) > 0)
+        {
+            DECLARE_ZERO_INIT_ARRAY(char, fieldValue,
+                                    (MAX_INT64_TO_DEC_STRING_LENGHT +
+                                     MAX_RAW_FEILD_UNIT_STRING_LENGTH)); // to be able to hold the unit string
+            snprintf_err_handle(fieldValue, (MAX_INT64_TO_DEC_STRING_LENGHT + MAX_RAW_FEILD_UNIT_STRING_LENGTH),
+                                "%" PRId64 " %s", smartAnalyzedAttribute.rawData.rawField[field].fieldValue,
+                                unitString);
+            json_object_object_add(rawDataNode, smartAnalyzedAttribute.rawData.rawField[field].fieldName,
+                                   json_object_new_string(fieldValue));
         }
         else
         {
-            json_object_object_add(
-                attributeNode, "Threshold Value",
-                json_object_new_uint64(C_CAST(uint64_t, smartAnalyzedAttribute.thresholdInfo.thresholdValue)));
+            DECLARE_ZERO_INIT_ARRAY(char, fieldValue, MAX_INT64_TO_DEC_STRING_LENGHT);
+            snprintf_err_handle(fieldValue, MAX_INT64_TO_DEC_STRING_LENGHT, "%" PRId64 "",
+                                smartAnalyzedAttribute.rawData.rawField[field].fieldValue);
+            json_object_object_add(rawDataNode, smartAnalyzedAttribute.rawData.rawField[field].fieldName,
+                                   json_object_new_string(fieldValue));
         }
-        break;
-    case THRESHOLD_UNKNOWN:
-    default:
-        break;
+        safe_free(&unitString);
     }
-
-    // add raw data fields
-    if (smartAnalyzedAttribute.rawData.userFieldCount > 0 || smartAnalyzedAttribute.rawData.boolAnalyzedValueValid ||
-        smartAnalyzedAttribute.rawData.doubleAnalyzedValueValid ||
-        smartAnalyzedAttribute.rawData.int64AnalyzedValueValid)
+    // add analyzed value in double
+    if (smartAnalyzedAttribute.rawData.doubleAnalyzedValueValid)
     {
-        json_object* rawDataNode = json_object_new_object();
-        // add raw fields
-        for (uint8_t field = 0; field < smartAnalyzedAttribute.rawData.userFieldCount; field++)
+        char* unitString = M_REINTERPRET_CAST(char*, safe_calloc(MAX_RAW_FEILD_UNIT_STRING_LENGTH, sizeof(char)));
+        get_Raw_Field_Unit_String(smartAnalyzedAttribute.rawData.doubleAnalyzedValueUnit, &unitString);
+        if (safe_strlen(unitString) > 0)
         {
-            char* unitString = M_REINTERPRET_CAST(char*, safe_calloc(MAX_RAW_FEILD_UNIT_STRING_LENGTH, sizeof(char)));
-            get_Raw_Field_Unit_String(smartAnalyzedAttribute.rawData.rawField[field].fieldUnit, &unitString);
-            if (safe_strlen(unitString) > 0)
-            {
-                DECLARE_ZERO_INIT_ARRAY(char, fieldValue,
-                                        (MAX_RAW_VALUE_OF_INT64_IN_STRING_LENGHT +
-                                         MAX_RAW_FEILD_UNIT_STRING_LENGTH)); // to be able to hold the unit string
-                snprintf_err_handle(
-                    fieldValue, (MAX_RAW_VALUE_OF_INT64_IN_STRING_LENGHT + MAX_RAW_FEILD_UNIT_STRING_LENGTH),
-                    "%" PRId64 " %s", smartAnalyzedAttribute.rawData.rawField[field].fieldValue, unitString);
-                json_object_object_add(rawDataNode, smartAnalyzedAttribute.rawData.rawField[field].fieldName,
-                                       json_object_new_string(fieldValue));
-            }
-            else
-            {
-                DECLARE_ZERO_INIT_ARRAY(char, fieldValue, MAX_RAW_VALUE_OF_INT64_IN_STRING_LENGHT);
-                snprintf_err_handle(fieldValue, MAX_RAW_VALUE_OF_INT64_IN_STRING_LENGHT, "%" PRId64 "",
-                                    smartAnalyzedAttribute.rawData.rawField[field].fieldValue);
-                json_object_object_add(rawDataNode, smartAnalyzedAttribute.rawData.rawField[field].fieldName,
-                                       json_object_new_string(fieldValue));
-            }
-            safe_free(&unitString);
+            DECLARE_ZERO_INIT_ARRAY(char, fieldValue,
+                                    (MAX_DOUBLE_TO_DEC_STRING_LENGHT +
+                                     MAX_RAW_FEILD_UNIT_STRING_LENGTH)); // to be able to hold the unit string
+            snprintf_err_handle(fieldValue, (MAX_DOUBLE_TO_DEC_STRING_LENGHT + MAX_RAW_FEILD_UNIT_STRING_LENGTH),
+                                "%f %s", smartAnalyzedAttribute.rawData.doubleAnalyzedValue, unitString);
+            json_object_object_add(rawDataNode, smartAnalyzedAttribute.rawData.doubleAnalyzedString,
+                                   json_object_new_string(fieldValue));
         }
-
-        // add analyzed value in double
-        if (smartAnalyzedAttribute.rawData.doubleAnalyzedValueValid)
+        else
         {
-            char* unitString = M_REINTERPRET_CAST(char*, safe_calloc(MAX_RAW_FEILD_UNIT_STRING_LENGTH, sizeof(char)));
-            get_Raw_Field_Unit_String(smartAnalyzedAttribute.rawData.doubleAnalyzedValueUnit, &unitString);
-            if (safe_strlen(unitString) > 0)
-            {
-                DECLARE_ZERO_INIT_ARRAY(char, fieldValue,
-                                        (MAX_RAW_VALUE_OF_DOUBLE_IN_STRING_LENGHT +
-                                         MAX_RAW_FEILD_UNIT_STRING_LENGTH)); // to be able to hold the unit string
-                snprintf_err_handle(fieldValue,
-                                    (MAX_RAW_VALUE_OF_DOUBLE_IN_STRING_LENGHT + MAX_RAW_FEILD_UNIT_STRING_LENGTH),
-                                    "%f %s", smartAnalyzedAttribute.rawData.doubleAnalyzedValue, unitString);
-                json_object_object_add(rawDataNode, smartAnalyzedAttribute.rawData.doubleAnalyzedString,
-                                       json_object_new_string(fieldValue));
-            }
-            else
-            {
-                DECLARE_ZERO_INIT_ARRAY(char, fieldValue, MAX_RAW_VALUE_OF_DOUBLE_IN_STRING_LENGHT);
-                snprintf_err_handle(fieldValue, MAX_RAW_VALUE_OF_DOUBLE_IN_STRING_LENGHT, "%f",
-                                    smartAnalyzedAttribute.rawData.doubleAnalyzedValue);
-                json_object_object_add(rawDataNode, smartAnalyzedAttribute.rawData.doubleAnalyzedString,
-                                       json_object_new_string(fieldValue));
-            }
-            safe_free(&unitString);
+            DECLARE_ZERO_INIT_ARRAY(char, fieldValue, MAX_DOUBLE_TO_DEC_STRING_LENGHT);
+            snprintf_err_handle(fieldValue, MAX_DOUBLE_TO_DEC_STRING_LENGHT, "%f",
+                                smartAnalyzedAttribute.rawData.doubleAnalyzedValue);
+            json_object_object_add(rawDataNode, smartAnalyzedAttribute.rawData.doubleAnalyzedString,
+                                   json_object_new_string(fieldValue));
         }
-
-        // add analyzed value in int64_t
-        if (smartAnalyzedAttribute.rawData.int64AnalyzedValueValid)
-        {
-            char* unitString = M_REINTERPRET_CAST(char*, safe_calloc(MAX_RAW_FEILD_UNIT_STRING_LENGTH, sizeof(char)));
-            get_Raw_Field_Unit_String(smartAnalyzedAttribute.rawData.int64AnalyzedValueUnit, &unitString);
-            if (safe_strlen(unitString) > 0)
-            {
-                DECLARE_ZERO_INIT_ARRAY(char, fieldValue,
-                                        (MAX_RAW_VALUE_OF_INT64_IN_STRING_LENGHT +
-                                         MAX_RAW_FEILD_UNIT_STRING_LENGTH)); // to be able to hold the unit string
-                snprintf_err_handle(fieldValue,
-                                    (MAX_RAW_VALUE_OF_INT64_IN_STRING_LENGHT + MAX_RAW_FEILD_UNIT_STRING_LENGTH),
-                                    "%" PRId64 " %s", smartAnalyzedAttribute.rawData.int64AnalyzedValue, unitString);
-                json_object_object_add(rawDataNode, smartAnalyzedAttribute.rawData.int64AnalyzedString,
-                                       json_object_new_string(fieldValue));
-            }
-            else
-            {
-                DECLARE_ZERO_INIT_ARRAY(char, fieldValue, MAX_RAW_VALUE_OF_INT64_IN_STRING_LENGHT);
-                snprintf_err_handle(fieldValue, MAX_RAW_VALUE_OF_INT64_IN_STRING_LENGHT, "%" PRId64 "",
-                                    smartAnalyzedAttribute.rawData.int64AnalyzedValue);
-                json_object_object_add(rawDataNode, smartAnalyzedAttribute.rawData.int64AnalyzedString,
-                                       json_object_new_string(fieldValue));
-            }
-            safe_free(&unitString);
-        }
-
-        // add analyzed value in boolean
-        if (smartAnalyzedAttribute.rawData.boolAnalyzedValueValid)
-        {
-            json_object_object_add(rawDataNode, smartAnalyzedAttribute.rawData.boolAnalyzedString, M_NULLPTR);
-        }
-
-        // add raw 0:7 bytes in hex
-        DECLARE_ZERO_INIT_ARRAY(char, rawDataValue, MAX_RAW_DATA_VALUE_IN_HEX_LENGTH);
-        snprintf_err_handle(rawDataValue, MAX_RAW_DATA_VALUE_IN_HEX_LENGTH,
-                            "%02" PRIX8 "%02" PRIX8 "%02" PRIX8 "%02" PRIX8 "%02" PRIX8 "%02" PRIX8 "%02" PRIX8 "h",
-                            smartAnalyzedAttribute.rawData.rawData[6], smartAnalyzedAttribute.rawData.rawData[5],
-                            smartAnalyzedAttribute.rawData.rawData[4], smartAnalyzedAttribute.rawData.rawData[3],
-                            smartAnalyzedAttribute.rawData.rawData[2], smartAnalyzedAttribute.rawData.rawData[1],
-                            smartAnalyzedAttribute.rawData.rawData[0]);
-        json_object_object_add(rawDataNode, "Raw Data", json_object_new_string(rawDataValue));
-
-        json_object_object_add(attributeNode, "Raw Data Information", rawDataNode);
+        safe_free(&unitString);
     }
-    else
+    // add analyzed value in int64_t
+    if (smartAnalyzedAttribute.rawData.int64AnalyzedValueValid)
     {
-        // add raw 0:7 bytes in hex
-        DECLARE_ZERO_INIT_ARRAY(char, rawDataValue, MAX_RAW_DATA_VALUE_IN_HEX_LENGTH);
-        snprintf_err_handle(rawDataValue, MAX_RAW_DATA_VALUE_IN_HEX_LENGTH,
-                            "%02" PRIX8 "%02" PRIX8 "%02" PRIX8 "%02" PRIX8 "%02" PRIX8 "%02" PRIX8 "%02" PRIX8 "h",
-                            smartAnalyzedAttribute.rawData.rawData[6], smartAnalyzedAttribute.rawData.rawData[5],
-                            smartAnalyzedAttribute.rawData.rawData[4], smartAnalyzedAttribute.rawData.rawData[3],
-                            smartAnalyzedAttribute.rawData.rawData[2], smartAnalyzedAttribute.rawData.rawData[1],
-                            smartAnalyzedAttribute.rawData.rawData[0]);
-        json_object_object_add(attributeNode, "Raw Data", json_object_new_string(rawDataValue));
+        char* unitString = M_REINTERPRET_CAST(char*, safe_calloc(MAX_RAW_FEILD_UNIT_STRING_LENGTH, sizeof(char)));
+        get_Raw_Field_Unit_String(smartAnalyzedAttribute.rawData.int64AnalyzedValueUnit, &unitString);
+        if (safe_strlen(unitString) > 0)
+        {
+            DECLARE_ZERO_INIT_ARRAY(char, fieldValue,
+                                    (MAX_INT64_TO_DEC_STRING_LENGHT +
+                                     MAX_RAW_FEILD_UNIT_STRING_LENGTH)); // to be able to hold the unit string
+            snprintf_err_handle(fieldValue, (MAX_INT64_TO_DEC_STRING_LENGHT + MAX_RAW_FEILD_UNIT_STRING_LENGTH),
+                                "%" PRId64 " %s", smartAnalyzedAttribute.rawData.int64AnalyzedValue, unitString);
+            json_object_object_add(rawDataNode, smartAnalyzedAttribute.rawData.int64AnalyzedString,
+                                   json_object_new_string(fieldValue));
+        }
+        else
+        {
+            DECLARE_ZERO_INIT_ARRAY(char, fieldValue, MAX_INT64_TO_DEC_STRING_LENGHT);
+            snprintf_err_handle(fieldValue, MAX_INT64_TO_DEC_STRING_LENGHT, "%" PRId64 "",
+                                smartAnalyzedAttribute.rawData.int64AnalyzedValue);
+            json_object_object_add(rawDataNode, smartAnalyzedAttribute.rawData.int64AnalyzedString,
+                                   json_object_new_string(fieldValue));
+        }
+        safe_free(&unitString);
     }
+    // add analyzed value in boolean
+    if (smartAnalyzedAttribute.rawData.stringAnalyzedValueValid)
+    {
+        json_object_object_add(rawDataNode, smartAnalyzedAttribute.rawData.stringAnalyzedString,
+                               json_object_new_string(smartAnalyzedAttribute.rawData.stringAnalyzedValue));
+    }
+    json_object_object_add(attributeNode, "Raw Data Information", rawDataNode);
 
     DECLARE_ZERO_INIT_ARRAY(char, statisticName, MAX_ATTRIBUTE_NODE_NAME_LENGTH);
     snprintf_err_handle(statisticName, MAX_ATTRIBUTE_NODE_NAME_LENGTH, "Attribute # %" PRIu8,
