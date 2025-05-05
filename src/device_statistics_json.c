c6212c0c5b50e4ec206099968ea1a1a9b9f1284f
Nidhi Sharma <nidhi.sharma@seagate.com>
2025-05-05 19:07:51 +0530

    Quick : updating copyright messages.

--- src/device_statistics_json.c
+++ src/device_statistics_json.c
@@ -1,19 +1,15 @@
+// SPDX-License-Identifier: MPL-2.0
 //
-// device_statistics_json.c
+// Do NOT modify or remove this copyright and license
 //
-// Do NOT modify or remove this copyright and confidentiality notice.
+// Copyright (c) 2012-2025 Seagate Technology LLC and/or its Affiliates, All Rights Reserved
 //
-// Copyright (c) 2012-2024 Seagate Technology LLC and/or its Affiliates, All Rights Reserved.
+// This software is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
 //
-// The code contained herein is CONFIDENTIAL to Seagate Technology LLC
-// and may be covered under one or more Non-Disclosure Agreements.
-// All or portions are also trade secret.
-// Any use, modification, duplication, derivation, distribution or disclosure
-// of this code, for any reason, not expressly authorized is prohibited.
-// All other rights are expressly reserved by Seagate Technology LLC.
+// ******************************************************************************************
 //
-// *****************************************************************************
-
 // \file device_statistics_json.c
 // \brief This file defines types and functions related to the JSON-based output for Device Statistics log.
 
