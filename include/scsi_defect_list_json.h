a42d00183d2ad9c14249aa8db120208c221e200e
ankur.d.gaikwad@seagate.com <ankur.d.gaikwad@seagate.com>
2025-08-08 15:55:35 +0530

    Quick: added support for scsi defect list in json

---
+++ include/scsi_defect_list_json.h
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MPL-2.0
+//
+// Do NOT modify or remove this copyright and license
+//
+// Copyright (c) 2012-2025 Seagate Technology LLC and/or its Affiliates, All Rights Reserved
+//
+// This software is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+// ******************************************************************************************
+//
+// \file device_statistics_json.h
+// \brief This file defines types and functions related to the JSON-based outptu for Device Statistics log.
+
+#pragma once
+
+#include "common_public.h"
+#include "common_types.h"
+#include "device_statistics.h"
+#include "defect.h"
+#include "jsonformat_common.h"
+#include "seagate_operations.h"
+
+#if defined(__cplusplus)
+extern "C"
+{
+#endif
+
+M_NONNULL_PARAM_LIST(1, 2)
+    M_PARAM_RO(1)
+    M_PARAM_RW(2)
+
+    OPENSEA_OPERATIONS_API eReturnValues create_JSON_Output_For_SCSI_Defect_List(ptrSCSIDefectList defects,
+                                                                                 char**           jsonFormat);
+
+#if defined(__cplusplus)
+}
+#endif
